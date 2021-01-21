24217979d87ad088278ec940633c54482567f808
Nihar Singla <nihar.singla@mettl.com>
2021-01-21 16:14:36 +0530

    react video player project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import ReactPlayer from 'react-player'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactPlayer controls width= '480px' height= '240px' 
+      url='https://www.youtube.com/watch?v=7sDY4m8KNLc&list=PLC3y8-rFHvwhAh1ypBvcZLDO6I7QTY5CM&index=12'
+      onReady={() => console.log('onReady callback')}
+      onStart={() => console.log('onStart callback')}
+      onPause={() => console.log('onPause callback')}
+      onEnded={() => console.log('onEnded callback')}
+      onError={() => console.log('onError callback')}
+      />
     </div>
   );
 }
